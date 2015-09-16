1ab251e9284faab497d17036c54884b3be2aa205
Seren <uemit.seren@gmi-lws06.gmi.oeaw.ac.at>
2015-09-16 15:50:24 +0200

    Updated tests

--- tests/test_genotype.py
+++ tests/test_genotype.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy
 
 
 class TestGenotype:
@@ -44,6 +45,37 @@ class TestGenotype:
 		snps = geno.get_snp_at(5,14252114)
 		assert snps is None
 
+
+	def test_get_snps_from_pos_filtered(self,geno):
+		accession_ix = range(0,1200)
+		geno.filter_accessions_ix(accession_ix)
+		chr_pos = [(1,572643),(3,11283260),(3,11283261),(1,16036830),(1,16036831),(5,14252117),(5,14252114),(4,8734565),(4,8734567),(2,9539640),(2,9539641)]
+
+		retval = geno.get_snps_from_pos(chr_pos)
+		assert len(retval) == 2
+		assert len(retval[0]) == len(retval[1])
+		assert len(retval[0]) == 6
+		indices = retval[0]
+		snps = retval[1]
+		assert indices == [1000, 98313, 25105, 180388, 136915, 63848]
+
+		assert sum(snps[0]) == 627
+		assert snps[0][753] == 0
+
+		assert sum(snps[1]) == 314
+		assert snps[1][886] == 1
+
+		assert sum(snps[2]) == 525
+		assert snps[2][912] == 1
+
+		assert sum(snps[3]) == 269
+		assert snps[3][727] == 1
+
+		assert sum(snps[4]) == 870
+		assert snps[4][890] == 0
+
+		assert sum(snps[5]) == 83
+		assert snps[5][963] == 1
 
 
 	def test_get_snps_from_pos(self,geno):
